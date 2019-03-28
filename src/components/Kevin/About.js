d52c0433ef06159240b313051d609aab94586bc2
Kevin Ung <ung.kevin78@gmail.com>
2019-03-28 15:44:41 -0600

    about mobile responsive

--- src/components/Kevin/About.js
+++ src/components/Kevin/About.js
@@ -1,16 +1,16 @@
 import React from 'react';
 
-const body = {background:'silver', width:'100%',height:457, marginTop:85,display:"flex",flexDirection:'column',alignItems:'center',borderLeft:'solid navy', borderRight:'solid navy'}
+// const body = {background:'silver', width:'100vw',minHeight:457, marginTop:85,display:"flex",flexDirection:'column',alignItems:'center',borderLeft:'solid navy', borderRight:'solid navy'}
 const header = {letterSpacing:'0.05em',background:'navy', width:'100vw',textAlign:'center',display:'flex',alignItems:'center',justifyContent:'center',minHeight:50,color:'white'}
-const text = {textAlign:'center',fontSize:35,width:310}
+// const text = {textAlign:'center',fontSize:35,width:310}
 
 
 export default function About () {
     return(
-        <div style={body}>
+        <div className="entire-about-section">
             <h1 style={header}>ABOUT</h1>
             <br/>
-            <p style={text}>MarketIn is a professional portfolio service to showcase your experiences, services, and connect with people.</p>
+            <p>MarketIn is a professional portfolio service to showcase your experiences, services, and connect with people.</p>
             <br/>
         </div>
     )
