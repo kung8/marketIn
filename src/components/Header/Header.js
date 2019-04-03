337c685d05405b0ce7273420c1c1438ac1813065
Kevin Ung <ung.kevin78@gmail.com>
2019-04-03 08:29:36 -0600

    attempted responsiveness for header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -11,10 +11,33 @@ class Header extends Component {
         this.state={
             navBar:0,
             isNavBarOpened:false,
-            // bigWindow:false
+            winWidth:0,
+            headerBar:false
         }
     }
     
+    componentDidMount(){
+        this.resize()   
+    }
+    
+    // windowResize(){
+    //         this.setState({
+    //             winWidth:window.screen.width
+    //         })
+    //         console.log(this.state.winWidth)
+    // }
+
+    resize(prevState,currState){
+        console.log('hit')
+        currState = window.screen.width;
+        prevState = this.state.winWidth;
+        if(prevState !== currState){
+            this.setState({
+                winWidth:window.screen.width
+            })
+        }
+    }
+
     toggleNavBar(){
         const {isNavBarOpened} = this.state;
         if(isNavBarOpened===false){
@@ -40,9 +63,53 @@ class Header extends Component {
         this.props.history.push('/'); 
 
     }
+
+    handleToggle=()=>{
+        console.log('hit!')
+        console.log(this.state.headerBar)
+        this.setState({
+            headerBar:!this.state.headerBar
+        })
+    }
+
+    toggleHeaderBar=()=>{
+        if(this.state.headerBar){
+            console.log(this.props.id)
+            if(this.props.id && this.state.headerBar > 700){
+                return(
+                    <div>
+                        <Link to={`/profile/${this.props.id}`}>{this.props.firstName} {this.props.lastName}</Link>
+                        <Link to={`/payments/${this.props.id}`} onClick={this.handleToggle}>Payments</Link>
+                        <Link to={`/services/${this.props.viewedUserId}`} onClick={this.handleToggle}>Services</Link>
+                        <Link to={`/contact/${this.props.viewedUserId}`} onClick={this.handleToggle}>Contact</Link>
+                        <span onClick={this.logout}>Logout</span>
+                    </div>
+                )
+    
+            } else {
+                console.log('should')
+                return(
+                    <div className="header-bar">
+                        <Link to='/about' onClick={this.handleToggle}><h1>About</h1></Link>
+                        <Link to='/profile/70' onClick={this.handleToggle}><h1>Dev</h1></Link>
+                        <Link to={`/services/70`} onClick={this.handleToggle}><h1>Services</h1></Link>
+                        <Link to={`/contact/70`} onClick={this.handleToggle}><h1>Contact</h1></Link>
+                        <Link to='/' onClick={this.handleToggle}><h1>Login</h1></Link>
+                    </div>
+                )
+            }
+
+        }
+    }
+
     render(){
         const {id} = this.props;
-        return(
+        this.resize()
+        console.log(this.state.winWidth)
+        const {winWidth} = this.state;
+        return (<div>
+
+            {(winWidth < 700) ? 
             <div className="header-container">
                 <div className="header">
                     <button style={{background:'unset'}} className="marketin-logo" onClick={()=>{this.toggleNavBar()}}>MI</button>
@@ -57,24 +124,32 @@ class Header extends Component {
                                 <p style={{color:'white',marginLeft:10,marginTop:10}}>Viewing:</p>
                                 <Link to={`/profile/${this.props.viewedUserId}`} onClick={()=>this.toggleNavBar()}>{this.props.userFirstName} {this.props.userLastName}</Link>
                                 <Link to={`/services/${this.props.viewedUserId}`} onClick={()=>this.toggleNavBar()}>Services</Link>
-                                {/* <a href={`mailto:${this.props.userEmail}`} onClick={()=>this.toggleNavBar()}>Contact</a> */}
                                 <Link to={`/contact/${this.props.viewedUserId}`} onClick={()=>this.toggleNavBar()}>Contact</Link>
                                 <span style={{color: 'white'}} onClick={this.logout}>Logout</span>
-                                {/* <Link to="/"><span style={{color: 'white'}} onClick={this.logout}>Logout</span></Link> */}
                         </div>
                         ) : (<div className="sidebar-container" style={{width:this.state.navBar}}>
                                 <Link to='/' onClick={()=>this.toggleNavBar()}>Login</Link>
                                 <Link to='/search' onClick={()=>this.toggleNavBar()}>Search</Link>
                                 <Link to='/about' style={{color:'white'}} onClick={()=>this.toggleNavBar()}>About MarketIn</Link>
-                                <Link to='/profile/70'onClick={()=>this.toggleNavBar()}>About the Dev</Link>
+                                <Link to='/profile/70' onClick={()=>this.toggleNavBar()}>About the Dev</Link>
                                 <Link to={`/services/70`} onClick={()=>this.toggleNavBar()}>Dev Services</Link>
                                 <Link to={`/contact/70`} onClick={()=>this.toggleNavBar()}>Contact the Dev</Link> 
-                                {/* <a href="mailto:ung.kevin78@gmail.com" onClick={()=>this.toggleNavBar()}>Contact</a> */}
                             </div>
                         )
                     }
-            </div>
-            
+            </div> 
+            :
+            <div className="header-container">
+                <div className="header" style={{display:'flex', justifyContent:'space-between',width:'96%'}}>
+                    <h1 className="marketin-name">MarketIn</h1>
+                    <Link to='/search'><i style={{color:'white',fontSize:35}} className="fas fa-search"/></Link>
+                    <i style={{color:'white', fontSize:35}} class="fas fa-bars" onClick={this.handleToggle}></i> 
+                    {this.state.headerBar && this.toggleHeaderBar()}
+                </div>
+            </div>}
+        </div> 
+
+        
         )
     }
 }
